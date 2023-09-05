9bc43b8402e5b615d97b1e138593c23e7d027db5
augusto.pinho <augusto.freitas@fatec.sp.gov.br>
2023-09-05 10:20:17 -0300

    ap8(bloco_anonimos): codigo 2.5.1

--- script.sql
+++ script.sql
@@ -7,11 +7,11 @@ DECLARE
 	n4 INT := -5;
 BEGIN
 	--divisão inteira
-	RAISE NOTICE '% / % = %', n1, n2, n1 / n2
+	RAISE NOTICE '% / % = %', n1, n2, n1 / n2;
 	--divisão real
 	RAISE NOTICE '% / % = %', n3, n2, to_char( n3 / n2, '99.99');
 	--resto da divisão
-	RAISE NOTICE '% %% % = %', n1, n2, n % n2;
+	RAISE NOTICE '% %% % = %', n1, n2, n1 % n2;
 	--exponenciação
 	RAISE NOTICE '% ^ % = %', n1, n2, n1 ^n2;
 	--raiz quadrada
